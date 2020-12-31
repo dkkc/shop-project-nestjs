c4a41e98aa47bcf5fad14f9fae01707c15e1deb0
Dariusz Kowalec <dariusz.kowalec@billennium.com>
2020-12-31 19:01:35 +0100

    added services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { BasketController } from './basket/basket.controller';
+import { ShopController } from './shop/shop.controller';
 import { AppService } from './app.service';
+import { BasketService } from './basket/basket.service';
+import { ShopService } from './shop/shop.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BasketController, ShopController],
+  providers: [AppService, BasketService, ShopService],
 })
 export class AppModule {}
